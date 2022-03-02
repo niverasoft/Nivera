be8224eee53a501fd8aa58fa1764e37c9c2bed88
Kenobi <77633241+genkenobidev@users.noreply.github.com>
2022-03-02 09:32:26 +0100

    Add the currently executing method to logs.
    I wrote this on my phone .. never doing that again

--- AtlasLib/Utils/AtlasHelper.cs
+++ AtlasLib/Utils/AtlasHelper.cs
@@ -1,10 +1,34 @@
+﻿using System.Reflection;
+
 ﻿namespace AtlasLib.Utils
 {
     internal static class AtlasHelper
     {
-        internal static void Info(object message)
+        private static Dictionary<MethodBase, string> _functionNames = new Dictionary<MethodBase, string>();)
+        private static MethodBase _curFunction;
+        private static string _curFunctionString;
+        
+        private static void SetFunctionString(MethodBase nextFunc)
         {
-            LibProperties.Logger?.Info(message.ToString());
+          if (_curFunction == null || nextFunc != _curFunction)
+          {
+            _curFunction = nextFunc;
+            _curFunctionString = $"{nextFunction.DeclaringType.FullName}::{nextFunction.Name}({string.Join(", ", nextFunc.GetParameters().Select(x => $"{x.ParameterType.FullName} {x.Name}"))})";
+          }
+        }
+        
+        internal static void Info(object message)
+        { 
+            string msg = message.ToString();
+            
+            if (LibProperties.ShowCurrentFunctionInLog)
+            {
+               SetFunctionString(new StackTrace().GetFrame(1).Method);
+               
+               msg += $"From Function: {_curFunctionString}";
+            }
+            
+            LibProperties.Logger?.Info(msg);
         }
 
         internal static void Debug(object message)
@@ -12,17 +36,44 @@
             if (!LibProperties.EnableDebugLog)
                 return;
 
-            LibProperties.Logger?.Debug(message.ToString());
+            string msg = message.ToString();
+            
+            if (LibProperties.ShowCurrentFunctionInLog)
+            {
+               SetFunctionString(new StackTrace().GetFrame(1).Method);
+               
+               msg += $"From Function: {_curFunctionString}";
+            }
+            
+            LibProperties.Logger?.Debug(msg);
         }
 
         internal static void Error(object message)
         {
-            LibProperties.Logger?.Error(message.ToString());
+            string msg = message.ToString();
+            
+            if (LibProperties.ShowCurrentFunctionInLog)
+            {
+               SetFunctionString(new StackTrace().GetFrame(1).Method);
+               
+               msg += $"From Function: {_curFunctionString}";
+            }
+            
+            LibProperties.Logger?.Error(msg);
         }
 
         internal static void Fatal(object message)
         {
-            LibProperties.Logger?.Fatal(message.ToString());
+            string msg = message.ToString();
+            
+            if (LibProperties.ShowCurrentFunctionInLog)
+            {
+               SetFunctionString(new StackTrace().GetFrame(1).Method);
+               
+               msg += $"From Function: {_curFunctionString}";
+            }
+            
+            LibProperties.Logger?.Fatal(msg);
         }
 
         internal static void Verbose(object message)
@@ -30,12 +81,30 @@
             if (!LibProperties.EnableVerboseLog)
                 return;
 
-            LibProperties.Logger?.Trace(message.ToString());
+            string msg = message.ToString();
+            
+            if (LibProperties.ShowCurrentFunctionInLog)
+            {
+               SetFunctionString(new StackTrace().GetFrame(1).Method);
+               
+               msg += $"From Function: {_curFunctionString}";
+            }
+            
+            LibProperties.Logger?.Verbose(msg);
         }
 
         internal static void Warn(object message)
         {
-            LibProperties.Logger?.Warn(message.ToString());
+            string msg = message.ToString();
+            
+            if (LibProperties.ShowCurrentFunctionInLog)
+            {
+               SetFunctionString(new StackTrace().GetFrame(1).Method);
+               
+               msg += $"From Function: {_curFunctionString}";
+            }
+            
+            LibProperties.Logger?.Warn(msg);
         }
     }
 }
